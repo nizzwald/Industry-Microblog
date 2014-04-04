1c40709e2397a5b2210996c15e1150ae66a01e62
John Fox <fox@johnfoxstudios.com>
2014-04-04 16:05:24 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 //Created by Jisko 3.0 auto-installer
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
-define('DB_USER', 'tip_big');
-define('DB_PASSWORD', 'h3r3again');
+define('DB_USER', 'XXX');
+define('DB_PASSWORD', 'XXX');
 define('DB_NAME', 'bigtippers');
 
 $globals['menubar_links'] = array();
